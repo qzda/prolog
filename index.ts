d55d2fe7613aeb6cb784441855607146684674ac
qzda <qinzhidaoa@gmail.com>
2024-08-20 22:29:26 +0800

    feat: update export

--- index.ts
+++ index.ts
@@ -1,31 +1,60 @@
-import { Backgrounds, Colors } from "./config";
+const Colors = {
+  black: 30,
+  red: 31,
+  green: 32,
+  yellow: 33,
+  blue: 34,
+  magenta: 35,
+  cyan: 36,
+  white: 37,
+  brightBlack: 90,
+  brightRed: 91,
+  brightGreen: 92,
+  brightYellow: 93,
+  brightBlue: 94,
+  brightMagenta: 95,
+  brightCyan: 96,
+  brightWhite: 97,
+} as const;
+
+const Backgrounds = {
+  bgBlack: 40,
+  bgRed: 41,
+  bgGreen: 42,
+  bgYellow: 43,
+  bgBlue: 44,
+  bgMagenta: 45,
+  bgCyan: 46,
+  bgWhite: 47,
+  bgBrightBlack: 100,
+  bgBrightRed: 101,
+  bgBrightGreen: 102,
+  bgBrightYellow: 103,
+  bgBrightBlue: 104,
+  bgBrightMagenta: 105,
+  bgBrightCyan: 106,
+  bgBrightWhite: 107,
+} as const;
 
 type Color = keyof typeof Colors;
 type Background = keyof typeof Backgrounds;
+type FuncParameterType = string | number;
 
-/**
- * Print color information on the console
- */
-function prolog(config: {
-  msg: string;
-  color?: Color;
-  bg?: Background;
-}): string {
-  let _msg = config.msg;
+const Obj = {
+  ...Object.keys(Colors).reduce((_obj, color) => {
+    _obj[color] = (str: FuncParameterType) =>
+      `\x1b[${Colors[color]}m${str}\x1b[0m`;
+    return _obj;
+  }, {}),
 
-  function getBgText(b: Background) {
-    return `\x1b[${Backgrounds[b]}m${_msg}\x1b[0m`;
-  }
+  ...Object.keys(Backgrounds).reduce((_obj, bg) => {
+    _obj[bg] = (str: FuncParameterType) =>
+      `\x1b[${Backgrounds[bg]}m${str}\x1b[0m`;
+    return _obj;
+  }, {}),
+} as {
+  [key in Color | Background]: (str: FuncParameterType) => string;
+};
 
-  function getColorText(c: Color) {
-    return `\x1b[${Colors[c]}m${_msg}\x1b[0m`;
-  }
-
-  if (config.color) _msg = getColorText(config.color);
-  if (config.bg) _msg = getBgText(config.bg);
-
-  return _msg;
-}
-
-export { Backgrounds, Colors };
-export default prolog;
+export default Obj;
+export { Colors, Backgrounds };
